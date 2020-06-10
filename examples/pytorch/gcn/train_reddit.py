052b70c5d4ec43edc3a2eeb9b1cefeea6b256de9
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-06-10 11:28:01 +0200

    change args

--- examples/pytorch/gcn/train_reddit.py
+++ examples/pytorch/gcn/train_reddit.py
@@ -122,19 +122,19 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='GCN')
-    parser.add_argument("--dropout", type=float, default=0.5,
+    parser.add_argument("--dropout", type=float, default=0.2,
             help="dropout probability")
-    parser.add_argument("--gpu", type=int, default=-1,
+    parser.add_argument("--gpu", type=int, default=0,
             help="gpu")
     parser.add_argument("--lr", type=float, default=1e-2,
             help="learning rate")
-    parser.add_argument("--n-epochs", type=int, default=200,
+    parser.add_argument("--n-epochs", type=int, default=30,
             help="number of training epochs")
-    parser.add_argument("--n-hidden", type=int, default=16,
+    parser.add_argument("--n-hidden", type=int, default=512,
             help="number of hidden gcn units")
     parser.add_argument("--n-layers", type=int, default=1,
             help="number of hidden gcn layers")
-    parser.add_argument("--weight-decay", type=float, default=5e-4,
+    parser.add_argument("--weight-decay", type=float, default=0.0,
             help="Weight for L2 loss")
     parser.add_argument("--self-loop", action='store_true',
             help="graph self-loop (default=False)")
