dfd1454ff0c4b716c3e89859d08c509be6410efb
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2020-06-11 19:06:45 +0200

    update playground

--- examples/pytorch/ogb/ogbn-products/playground/playground.py
+++ examples/pytorch/ogb/ogbn-products/playground/playground.py
@@ -6,8 +6,12 @@ import torch.nn.functional as F
 import torch.optim as optim
 from ogb.nodeproppred import DglNodePropPredDataset
 from dgl.nn.pytorch import SAGEConv
+from dgl.data.reddit import RedditDataset
 
 import subprocess
+import time
+import logging
+import sys
 
 
 class GraphSAGE(nn.Module):
@@ -40,20 +44,29 @@ class GraphSAGE(nn.Module):
 
 name = "playground"
 monitoring_gpu = subprocess.Popen(["nvidia-smi", "dmon", "-s", "umt", "-o", "T", "-f", f"{name}.smi"])
+logging.basicConfig(filename=f"{name}.log",level=logging.DEBUG)
+def log(start, when):
+    mb = 1e6
+    logging.debug("{:.1f}s:{}:active {:.2f}MB, allocated {:.2f}MB, reserved {:.2f}MB".format(time.time() - start, when, torch.cuda.memory_stats()["active_bytes.all.allocated"] / mb, torch.cuda.memory_allocated() / mb, torch.cuda.memory_reserved() / mb))
+start = time.time()
 
-dataset = DglNodePropPredDataset(name='ogbn-products') # list with one graph as element
-
-splitted_idx = dataset.get_idx_split() # dict
+data = DglNodePropPredDataset(name='ogbn-products') # list with one graph as element
+splitted_idx = data.get_idx_split() # dict
 train_idx, val_idx, test_idx = splitted_idx['train'], splitted_idx['valid'], splitted_idx['test'] # torch.Tensor
 graph = dataset.graph[0] # dgl.graph.DGLGraph
 num_classes = dataset.num_classes # int
 features = torch.FloatTensor(data.features)
 labels = torch.LongTensor(data.labels)
-
 feature_dim = features.size()[1]
 
-# graph.ndata dgl.view.NodeDataView
-# graph.ndata.keys() collections.abc.KeysView
+logging.info("Dataset:Reddit")
+logging.info("Number of nodes:{}".format(graph.number_of_nodes()))
+logging.info("Number of edges:{}".format(graph.number_of_edges()))
+logging.info("Dimensionality of features:{}".format(feature_dim))
+logging.info("Number of classes:{}".format(num_classes))
+logging.info("Number of training samples:{}".format(len(np.nonzero(train_idx)[0])))
+logging.info("Number of validation samples:{}".format(len(np.nonzero(val_idx)[0])))
+logging.info("Number of test samples:{}".format(len(np.nonzero(test_idx)[0])))
 
 num_hidden_channels = 128
 num_hidden_layers = 1
@@ -65,31 +78,37 @@ learning_reate = 3e-3
 optimizer = optim.Adam(model.parameters(), lr=learning_reate)
 loss_fn = nn.CrossEntropyLoss()
 
-mb = 1e6
+logging.info("Number of hidden channels:{}".format(num_hidden_channels))
+logging.info("Number of hidden layers:{}".format(num_hidden_layers))
+logging.info("Activation function:{}".format("F.relu"))
+logging.info("Dropout:{}".format(p_dropout))
+logging.info("Aggregation:{}".format(aggregator_type))
+logging.info("Model:{}".format("GraphSAGE"))
+logging.info("Optimizer:{}".format("optim.Adam"))
+logging.info("Loss function:{}".format("nn.CrossEntropyLoss"))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Memory reserved beginning: {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
+log(start, "after declaration")
 model.to(device)
-print("Memory reserved after model.to(): {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
+log(start, "after model.to()")
 features = features.to(device)
 labels = labels.to(device)
-print("Memory reserved after data.to(): {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
+log(start, "after data.to()")
 
 # training
-num_epochs = 3
 try:
-    for _ in range(num_epochs):
-        embeddings = model(features)
-        print("Memory reserved after forward: {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
-        loss = loss_fn(embeddings[train_idx], labels[train_idx])
-        print("Memory reserved after loss: {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
-        optimizer.zero_grad()
-        print("Memory reserved after zero_grad(): {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
-        loss.backward()
-        print("Memory reserved after backward: {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
-        optimizer.step()
-        print("Memory reserved after optimizer.step(): {:.2f} MB".format(torch.cuda.memory_reserved(device=device) / mb))
+    embeddings = model(features)
+    log(start, "after forward")
+    loss = loss_fn(embeddings[train_idx], labels[train_idx])
+    log(start, "after loss")
+    optimizer.zero_grad()
+    log(start, "after optimizer.zero_grad()")
+    loss.backward()
+    log(start, "after backward")
+    optimizer.step()
+    log(start, "after optimizer.zero_grad()")
 except:
-    print("Training failed")
-finally:
-    monitoring_gpu.terminate()
+    logging.error(sys.exc_info()[0])
+
+time.sleep(5)
+monitoring_gpu.terminate()
